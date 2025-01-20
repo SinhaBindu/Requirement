5db8f783516a26e03a1571f6430d939ccbd16cdd
Bindu Kumari <142012740+SinhaBindu@users.noreply.github.com>
2025-01-20 15:47:48 +0530

    Initial commit

---
+++ Requirement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Requirement.Startup))]
+namespace Requirement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
